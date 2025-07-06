fd19155e3745ac4bb9dc0cea6b0dc96dfbceafeb
Jatin Thakur <jtkalga@gmail.com>
2025-07-06 13:01:12 +0530

    feat: new type `Ruleset`

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,6 +2,9 @@ export interface ServerConfig {
   guildId: string;
   quarantineRoleId: string;
   verificationChannelId: string;
-  welcomeChannelId?: string;
-  verifiedRoleId?: string;
+  welcomeChannelId?: string | null;
+  verifiedRoleId?: string | null;
+  botPrefix?: string | null;
 }
+
+export type Ruleset = "osu" | "mania" | "taiko" | "fruits";
